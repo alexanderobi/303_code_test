5d949d789a485d7a66b06449a68f41f52ab42e37
Alexander Obi <alexander.obi@smartrac-group.com>
2018-07-24 16:58:56 -0400

    fix seed

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,12 +1,14 @@
 const faker = require('faker');
 const initData = require('./database');
 
-const stub = {
+const stub = () => ({
   title: faker.name.title(),
   author: `${faker.name.firstName()} ${faker.name.firstName()}`,
   content: faker.lorem.sentence(),
-};
+});
 
 initData().then(async ({ models: { test } }) => {
-  await Promise.all[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => test.create(stub))];
+  await Promise.all[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => test.create(stub()))];
+  console.log( "\nSeeding done!" );
+  process.exit(0);
 });
